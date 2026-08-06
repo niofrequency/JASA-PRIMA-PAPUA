bd7789438b7d3c61367e76c1d1b3cb3673c225d8
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-08-06 20:41:59 +0900

    Update generate-from-safetyculture.ts

--- api/ai/generate-from-safetyculture.ts
+++ api/ai/generate-from-safetyculture.ts
@@ -3,6 +3,7 @@ import { generateCourseFromSafetyCultureServer, type SCCourseFullRaw } from '../
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-store');
   res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
