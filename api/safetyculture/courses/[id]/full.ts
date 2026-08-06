6b91007e8025cc0c3d3d65a88c2f3f1ffbf3556c
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-08-06 18:16:37 +0900

    Update full.ts

--- api/safetyculture/courses/[id]/full.ts
+++ api/safetyculture/courses/[id]/full.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getFullCourse, SafetyCultureConfigError, SafetyCultureApiError } from '../../../../lib/safetyculture/client';
+import { getFullCourse, SafetyCultureConfigError, SafetyCultureApiError } from '../../../../lib/safetyculture/client.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
